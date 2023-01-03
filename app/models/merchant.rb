e9b9bd946adfa365c3ddcd48da2890d6090ef1c9
Moussa Diop <moussa.diop@akilee-by-ines.com>
2023-01-03 08:49:02 +0000

    Add merchant total transactions sum calculations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,16 @@ class Merchant < User
   has_many :charge_transactions, class_name: "ChargeTransaction", foreign_key: "merchant_id"
   has_many :refund_transactions, class_name: "RefundTransaction", foreign_key: "merchant_id"
   has_many :reversal_transactions, class_name: "ReversalTransaction", foreign_key: "merchant_id"
+
+  def receive!(amount)
+    self.total_transaction_sum += amount
+    save!
+  end
+
+  def refund!(amount)
+    self.total_transaction_sum -= amount
+    save!
+  end
 end
 
 # == Schema Information
@@ -14,6 +24,7 @@ end
 #  email                 :string           not null
 #  encrypted_password    :string           not null
 #  name                  :string           not null
+#  remember_created_at   :datetime
 #  status                :integer          default("inactive"), not null
 #  total_transaction_sum :decimal(, )      default(0.0), not null
 #  type                  :string
