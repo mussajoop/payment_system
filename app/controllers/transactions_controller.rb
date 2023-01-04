788187f1fe3e48ab2b86e1a2111281ed8bfbaa47
Moussa Diop <moussa.diop@akilee-by-ines.com>
2023-01-04 18:16:55 +0000

    Add transaction controller action to list transaction and process payments

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,24 @@
+class TransactionsController < ApplicationController
+  # GET /transactions
+  def index
+    authorize Transaction
+    @transactions = Transaction.all_for_current_user(current_user)    
+  end
+
+  # POST /transactions or /transactions.json
+  def payment
+    authorize Transaction
+    @transaction = TransactionService.authorize!(transaction_params.merge(merchant: current_user))
+
+    respond_to do |format|
+      format.xml { render xml: @transaction }
+      format.json { render json: @transaction }
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:uuid, :amount, :customer_email, :customer_phone)
+  end
+end
