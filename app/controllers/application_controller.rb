24ad9f110c0e3068ca72888e99a144128fa45fde
Moussa Diop <moussa.diop@akilee-by-ines.com>
2023-01-04 18:18:25 +0000

    Add authorization utilities(forgery protection, authentication and pundit)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  before_action :authenticate_user!
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  private
+
+  def not_authorized
+    flash[:notice] = "Unauthorized action!"
+    redirect_to(root_url)
+  end
 end
