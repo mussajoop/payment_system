6568123a6c72a6c60da7cf95488ba9fe8e29d3e7
Moussa Diop <moussa.diop@akilee-by-ines.com>
2023-01-04 18:31:18 +0000

    Update routes set default routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,20 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "transactions#index"
-  resources :transactions, only: [:index] do
+  authenticated :user do
+    root to: "transactions#index", as: :authenticated_user
+  end
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
+  resources :transactions do
     post :payment, on: :collection
   end
+
+  resources :merchants
 end
