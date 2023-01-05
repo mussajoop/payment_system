77e8beb97e1a0076283aeb27cfdef76fe0a6c037
Moussa Diop <moussa.diop@akilee-by-ines.com>
2023-01-05 06:21:10 +0000

    Clear sidekiq queue on deployment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,4 +6,8 @@ Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
 end
 
-Sidekiq::Cron::Job.create(name: 'CleanTransactionsJob - every hour', cron: '0 */1 * * *', class: 'CleanTransactionsJob') unless Rails.env == 'test'
+unless Rails.env == 'test'
+  require 'sidekiq/api'
+  Sidekiq::Queue.all.each &:clear
+  Sidekiq::Cron::Job.create(name: 'CleanTransactionsJob - every hour', cron: '0 */1 * * *', class: 'CleanTransactionsJob') unless Rails.env == 'test'
+end
